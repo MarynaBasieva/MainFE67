5fb09c87fbd4a48d2de34bc87a46c11f485d2a5d
Hfox22 <90965288+Hfox22@users.noreply.github.com>
2022-05-08 13:56:58 +0300

    HM

---
+++ HomeWork/VadymMorgyn/HomeWork/project 1/HM20/js/custom2.js
@@ -0,0 +1,63 @@
+jQuery.noConflict()
+jQuery(document).ready(function () {
+	;('use strict')
+
+	jQuery('.fancybox').fancybox()
+	/*Owl carousel1*/
+
+	// let carousel3 = jQuery('#testimonials_img')
+
+	// carousel3.owlCarousel({
+	// 	items: 1,
+	// 	autoplay: false,
+	// 	autoplayTimeout: 6000,
+	// 	autoplayHoverPause: true,
+	// 	smartSpeed: 1000,
+	// 	dotsSpeed: 1000,
+	// 	loop: false,
+	// 	nav: true,
+	// 	navText: [''],
+	// 	dots: false,
+	// 	mouseDrag: true,
+	// 	margin: 0,
+	// 	stagePadding: 0,
+	// 	autoWidth: false,
+	// 	responsiveClass: true,
+	// 	responsive: {
+	// 		0: {
+	// 			items: 1,
+
+	// 			nav: true,
+	// 			mouseDrag: true,
+	// 			autoplay: true,
+	// 			smartSpeed: 1000,
+	// 		},
+	// 		479: {
+	// 			items: 1,
+	// 			nav: true,
+	// 			mouseDrag: true,
+	// 			autoplay: true,
+	// 			smartSpeed: 1000,
+	// 		},
+	// 		767: {
+	// 			items: 1,
+	// 			nav: true,
+	// 			mouseDrag: true,
+	// 			autoplay: true,
+	// 			smartSpeed: 1000,
+	// 		},
+	// 		991: {
+	// 			items: 1,
+	// 			nav: true,
+	// 			mouseDrag: true,
+	// 			autoplay: true,
+	// 			smartSpeed: 1000,
+	// 		},
+	// 		1099: {
+	// 			items: 1,
+	// 			nav: true,
+	// 			mouseDrag: true,
+	// 		},
+	// 	},
+	// })
+})
