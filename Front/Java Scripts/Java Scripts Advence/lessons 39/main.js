f3c9acd4a9b0e73dd8283ebf2fa4aac5770a2a7f
Basieva Maryna <verna.job@gmail.com>
2022-07-13 20:11:02 +0300

    CW40

---
+++ Front/Java Scripts/Java Scripts Advence/lessons 39/main.js
@@ -0,0 +1,20 @@
+async function responseFetch(){
+	let response = await fetch('http://localhost:8888/photo.json');
+	let content = await response.json();
+	content = content.splice(0,8);
+	
+	let list = document.querySelector('.posts');
+	
+	let key;
+	
+	for(key in content){
+		list.innerHTML +=`
+			<li class="post">
+				<h2>${content[key].title}</h2>
+				<img src="${content[key].url}" width="300"/>
+			</li>
+	`
+	}
+}
+
+responseFetch()
